bc7b6244bef83192cdb9e7f4f0c2f443da137b6b
binding1 <jaey.pro@gmail.com>
2022-11-20 06:32:50 +0000

    nested all callback functions to complete functional ISS flyover time fetch

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
-});
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
-fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
 
 // fetchISSFlyOverTimes({latitude: -80, longitude: -23}, (error, responses) => {
 //   if (error) {
@@ -27,3 +28,19 @@ fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
 
 //   console.log('It worked! Returned flyover times:' , responses);
 // });
+
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  
+  for (let i in passTimes) {
+    const dateRisetime = new Date(0);
+    dateRisetime.setUTCSeconds(passTimes[i]['risetime']);
+    console.log(`Next pass at ${dateRisetime} for ${passTimes[i]['duration']} seconds.`);
+  };
+  
+});
--- iss.js
+++ iss.js
@@ -70,13 +70,21 @@ const fetchCoordsByIP = function(ip, callback) {
 
     if (data) {
       callback(null, coordinates);
-      return;
     }
   }
   );
 };
 
-
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
 const fetchISSFlyOverTimes = function(coordinates, callback) {
   request(`https://iss-flyover.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`, (error, response, body) => {
     if (error) {
@@ -102,13 +110,46 @@ const fetchISSFlyOverTimes = function(coordinates, callback) {
 
     if (data) {
       callback(null, flyOverTimes);
-      return;
     }
   });
 };
 
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, coordinates) => {
+      if (error) {
+        return callback(error, null);
+      }
+    
+      fetchISSFlyOverTimes(coordinates, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+                
+        callback(null, nextPasses)
+
+        });
+      });
+    });
+};
+
+
 module.exports = {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes
+  // fetchMyIP,
+  // fetchCoordsByIP,
+  // fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation
 };
