b131b4acaf8649148823fcaec6de0dbe30e9e2e5
binding1 <jaey.pro@gmail.com>
2022-11-20 05:53:37 +0000

    commented out flyover callout

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ fetchCoordsByIP('8.8.8.8', (error, coordinates) => {
   console.log('It worked! Returned coordinates:' , coordinates);
 });
 
-fetchISSFlyOverTimes({latitude: -80, longitude: -23}, (error, responses) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchISSFlyOverTimes({latitude: -80, longitude: -23}, (error, responses) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned flyover times:' , responses);
-});
+//   console.log('It worked! Returned flyover times:' , responses);
+// });
